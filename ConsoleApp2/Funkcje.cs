08153bf7f4cce65a03135462a89fc2313b51f4e0
Łukasz Babiuch <lukbabiuch@outlook.com>
2019-10-07 23:42:18 +0200

    Srednia uczniow

---
+++ ConsoleApp2/Funkcje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Funkcje
+    {
+        public float ObliczSrednia(float suma, int iloscElementow)
+        {
+            return (float)suma / iloscElementow;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿// obliczanie sredniej ocen, jeden przedmiot, nie wiadomo ile ocen
+
+using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            string text = "@";
+            float suma = 0;
+            float result;
+
+            Funkcje obiekt = new Funkcje();
+
+            while (text != string.Empty)
+            {
+                text = Console.ReadLine();
+                    builder.Append(text + ";");
+            }
+
+            string gradesString = builder.ToString();
+            string[] grades = gradesString.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            float[] gradesInt = new float[grades.Length];
+
+            for(int i = 0; i < grades.Length; i++)
+            {
+                gradesInt[i] = Convert.ToSingle(grades[i]);
+            }
+
+            foreach (float grade in gradesInt)
+            {
+                suma += grade;
+            }
+
+            Console.WriteLine(obiekt.ObliczSrednia(suma, gradesInt.Length));
+        }
+    }
+}
